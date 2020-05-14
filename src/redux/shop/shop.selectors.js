38c92b12dcad1668c9960cdb792f1fe44ec8b7a9
Andrei Muja <andrei.raiden20@gmail.com>
2020-05-14 21:00:24 +0300

    added redux thunk

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -15,3 +15,11 @@ export const selectCollection=collectionUrlParam => createSelector(
     //collections => collections.find(collection => collection.id===COLLECTION_ID_MAP[collectionUrlParam])
     collections => (collections ? collections[collectionUrlParam] : null)
 );
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop], shop=>shop.isFetching
+);
+
+export const selectIsCollectionLoaded = createSelector(
+    [selectShop], shop=>!!shop.collections
+);
